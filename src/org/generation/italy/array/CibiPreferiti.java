5761fb57a7d126e0c2617104f917d7f1ce9ca82c
loiam <loiamat2001@gmail.com>
2021-11-17 15:37:08 +0100

    bonus

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -6,10 +6,18 @@ public class CibiPreferiti {
 		
 		String[] cibiPreferiti= {"risotto alla milanese","lasagna","frittelle ai fiori di zucca","polenta","carbonara","focaccia","alette di pollo"};
 		
-		int lungezza=cibiPreferiti.length;
+		int lunghezza=cibiPreferiti.length;
 		
-		System.out.println("La classifica ha "+lungezza+" posizioni");
+		System.out.println("La classifica ha "+lunghezza+" posizioni");
 		System.out.println("Il mio cibo preferito è: "+cibiPreferiti[0]);
-		System.out.println("Il cibo all'ultima posizione è: "+cibiPreferiti[lungezza-1]);
+		System.out.println("Il cibo all'ultima posizione è: "+cibiPreferiti[lunghezza-1]);
+		
+		
+			if (lunghezza % 2 == 0) {
+				System.out.println("I cibi a metà classifica sono: \n"+(lunghezza/2)+"° posizione, "+cibiPreferiti[lunghezza/2-1]+"\n"+(lunghezza/2+1)+"° posizione, "+cibiPreferiti[lunghezza/2]);
+			}else {
+				System.out.println("Il cibo a metà classifica è: "+(lunghezza/2)+"° posizione, "+cibiPreferiti[lunghezza/2-1]);
+			}
+		
 	}
 }
